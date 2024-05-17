5bf5ade293c9d34549dfcc3309b332c6ea6a5970
SShehan <sachinshehan20@gmail.com>
2024-05-17 16:07:59 +0530

    ui updated

--- frontend/src/App/routes/index.js
+++ frontend/src/App/routes/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../Pages/Home';
 import AdminDashboard from '../Pages/AdminDashboard';
 
 const AppRoutes = () => {
